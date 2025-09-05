6c32f688af843d85ab7bdba8354f13fe8a53df82
Teju-Aws <tejaswinigundluru@gmail.com>
2025-09-05 14:32:41 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from colorama import Fore, Style, init
+
+# initialize colorama
+init(autoreset=True)
+
+# define color variables
+red   = Fore.RED
+green = Fore.GREEN
+blue  = Fore.BLUE
+yellow = Fore.YELLOW
+reset = Style.RESET_ALL
+
+# use them
+print(red + "This is red text" + reset)
+print(green + "This is green text" + reset)
+print(blue + "This is blue text" + reset)
+print(yellow + "This is yellow text" + reset)
+
+# mixing styles
+print(blue + Style.BRIGHT + "Bright Blue Text" + reset)
+
